8a60941427fbb317b945e124b1d7701ef4b59e7a
Ysaikumar5344 <168627478+Ysaikumar5344@users.noreply.github.com>
2024-07-11 18:17:29 +0530

    Project3.py

--- Project3.py
+++ Project3.py
@@ -4,7 +4,6 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 st.image(r'innomatics-footer-logo.webp')
 st.title("E-mail Spam or Ham Classification")
-st.image(r"spamorhamclassification.jpg")
 model=pickle.load(open(r"nb.pkl",'rb'))
 Vectorizer=pickle.load(open(r'count_vectorizer.pkl', 'rb'))
 
@@ -16,6 +15,6 @@ if Message:
     if st.button("Classify"):
         st.write("Classification(SPAM/HAM) Result: This is a",result.upper(),"Mail")  
         if result=="spam":
-            st.image(r"spammmm.png")
+            st.image(r"Spammimg.png")
         elif result=="ham":
-            st.image(r"hammmm.png")
+            st.image(r"Hamimg.png")
