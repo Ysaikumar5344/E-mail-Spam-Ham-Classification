1c5331b76d13ed962f0ace5c7fec1965f333ec42
Ysaikumar5344 <168627478+Ysaikumar5344@users.noreply.github.com>
2024-07-11 10:02:38 +0530

    Project3.py

--- Project3.py
+++ Project3.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 
-st.image(r'C:\Users\Y SAI KUMAR\Music\innomatics-footer-logo.webp')
+st.image(r'innomatics-footer-logo.webp')
 st.title("E-mail Spam or Ham Classification")
-st.image(r"C:\Users\Y SAI KUMAR\Desktop\spamorhamclassification.jpg")
-model=pickle.load(open(r"C:\Users\Y SAI KUMAR\New folder\nb.pkl",'rb'))
-Vectorizer=pickle.load(open(r"C:\Users\Y SAI KUMAR\New folder\count_vectorizer.pkl", 'rb'))
+st.image(r"spamorhamclassification.jpg")
+model=pickle.load(open(r"nb.pkl",'rb'))
+Vectorizer=pickle.load(open(r'count_vectorizer.pkl", 'rb'))
 
 Message=st.text_input("Enter the mail")
 if Message:
@@ -16,6 +16,6 @@ if Message:
     if st.button("Classify"):
         st.write("Classification(SPAM/HAM) Result: This is a",result.upper(),"Mail")  
         if result=="spam":
-            st.image(r"C:\Users\Y SAI KUMAR\Music\spammmm.png")
+            st.image(r"spammmm.png")
         elif result=="ham":
-            st.image(r"C:\Users\Y SAI KUMAR\Music\hammmm.png")
+            st.image(r"hammmm.png")
